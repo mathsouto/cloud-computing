61315b56d23e777e41d3a75226f6a6268e7c71b3
Matheus <tetisouto@gmail.com>
2024-11-07 14:34:39 -0300

    att

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ window.addEventListener("scroll",() => {
     header.classList.toggle("sticky", window.scrollY > 30)
 })
 
+//Menu mobile
+const btnMobile = document.getElementById('btn-mobile');
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    const body = document.body;
+    nav.classList.toggle('active'); //Adiciona caso não tenha, remova caso tenha
+    body.classList.toggle('no-scroll'); //Alterna o menu e bloqueia/desbloqueia o scroll
+}
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
 //Accordion click
 document.addEventListener('click', function(event) {
     const accordion = event.target.closest('.accordion');
@@ -18,11 +30,9 @@ ScrollReveal().reveal('.titleOne, .subtitleOne, .imgTwo, .textsTwo, .leftBox, .r
     duration: 2000,
     distance: '12.6%'
 });
-
 ScrollReveal().reveal('.titleOne, .imgTwo, .leftBox', {
     origin: 'left'
 });
-
 ScrollReveal().reveal('.subtitleOne, .textsTwo, .rightBox', {
     origin: 'right'
 });
